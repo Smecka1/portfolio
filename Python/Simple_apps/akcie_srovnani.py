cb967587a0d098f60324cee50edae0e4a32d9385
Kamil Vítek <polvitek@seznam.cz>
2024-12-12 13:40:12 +0100

    add an application for comparing the price development of two or more shares

---
+++ Python/Simple_apps/akcie_srovnani.py
@@ -0,0 +1,61 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import datetime
+
+
+while True:
+    # Zadej tikry porovnávaných akcií
+    stocks = input("zadej tikry (např.....ASML, LRCX, KLAC):\n").split(", ")
+
+    # Načti historická data do současnosti (zadej od kerého data)
+    start_date = input("Zadej datum od ve formátu: 2020-01-01:\n")
+    end_date = datetime.date.today().strftime('%Y-%m-%d')
+
+    data = yf.download(stocks, start=start_date, end=end_date)['Adj Close']
+    dividends = {stock: yf.Ticker(stock).dividends for stock in stocks}
+
+    # Normalizuj ceny na stejný bod (první řádek tabulky), aby graf začínal na stejné úrovni
+    normalized_data = data / data.iloc[0] * 100 - 100    # - 100 aby osa Y začínala na 0% ne 100%
+
+    # Vypočítání procentuální změny od začátku období
+    percent_changes = data.iloc[-1] / data.iloc[0] * 100 - 100
+    print(f"Procentuální pohyb za dané období:")
+    for ticker, change in sorted(percent_changes.items(), key=lambda item: item[1], reverse=True):
+        print(f"{ticker}: {change:.2f}%")
+    # Vypočítání celkové návratnosti i se započtenou dividendou
+    total_returns = {}
+    for stock in stocks:
+        total_dividends = dividends[stock].loc[start_date:end_date].sum()
+        total_value = data[stock].iloc[-1] + total_dividends  # Cena + dividendy
+        total_return = ((total_value / data[stock].iloc[0]) - 1) * 100
+        total_returns[stock] = total_return
+    print("Celková návratnost (včetně dividend):")
+    for stock, total_return in sorted(total_returns.items(), key=lambda item: item[1], reverse=True):
+        print(f"{stock}: {total_return:.2f}%")
+
+    # Vykresli graf
+    plt.figure(figsize=(12, 6))
+    for stock in stocks:
+        plt.plot(data.index, normalized_data[stock], label=stock)
+
+    for stock in stocks:
+        plt.text(
+            normalized_data.index[-1],  # x pozice: poslední datum v datech
+            normalized_data[stock].iloc[-1],  # y pozice: poslední hodnota pro daný stock
+            f'{percent_changes[stock]:.2f}%',  # Text popisku s procentuální změnou
+            fontsize=10,
+            ha='left'  # Umístí text mírně vpravo
+        )
+    plt.title("Procentuální nárust/pokles ceny za dané období")
+    plt.xlabel('Datum')
+    plt.ylabel('Pohyb ceny v %')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    print("\n")
+    end = input("Pro ukončení zadej: q   ")
+    if end == "q":
+        break
+
+
