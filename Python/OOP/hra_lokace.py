52a6ddf7b9323cf1d1424df2ec8f3a1dfef20045
Kamil Vítek <polvitek@seznam.cz>
2025-01-20 21:05:37 +0100

    better UX

--- Python/OOP/hra_lokace.py
+++ Python/OOP/hra_lokace.py
@@ -15,19 +15,19 @@ class Lokace:
         self.popis = popis
 
     def __str__(self):
-        return f"{self.misto}\n{self.popis}\n\nMůžeš jít na {", ".join(self.smery)}\n"
+        return f"{self.misto}\n{self.popis}\nMůžeš jít na {", ".join(self.smery)}\n"
 
     def kontrola(self, smer):
             return smer in self.smery
 
 
 class Hra:
-    hrad = Lokace(["východ"], "Hrad", "Stojíš před okovanou branou gotického hradu, která je zřejmě jediným vchodem do pevnosti. Klíčová dírka je pokryta pavučinami, což vzbuzuje dojem, že je budova opuštěná.")
-    les1 = Lokace(["západ", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.")
+    hrad = Lokace(["východ"], "Hrad", "Stojíš před okovanou branou gotického hradu, která je zřejmě jediným vchodem do pevnosti.\nKlíčová dírka je pokryta pavučinami, což vzbuzuje dojem, že je budova opuštěná.")
+    les1 = Lokace(["západ", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce.\nTicho podvečerního lesa občas přeruší zpěv posledních ptáků.")
     les2 = Lokace(["jih", "západ", "východ"], "Lesní rozcestí", "Nacházíš se na lesním rozcestí.")
-    les3 = Lokace(["západ", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.")
+    les3 = Lokace(["západ", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce.\nTicho podvečerního lesa občas přeruší zpěv posledních ptáků.")
     rybnik = Lokace(["západ"], "Rybník", "Došel jsi ke břehu malého rybníka. Hladina je v bezvětří jako zrcadlo. Kousek od tebe je dřevěná plošina se stavidlem.")
-    les4 = Lokace(["sever", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.")
+    les4 = Lokace(["sever", "východ"], "Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce.\nTicho podvečerního lesa občas přeruší zpěv posledních ptáků.")
     dum = Lokace(["západ"], "Dům", "Stojíš před svým rodným domem, citíš vůni čerstvě nasekaného dřeva, která se line z hromady vedle vstupních dvěří.")
 
     rozlozeni = ([hrad, les1, les2, les3, rybnik],
@@ -53,19 +53,19 @@ class Hra:
     def interakce(self):
         smer = " "
         while smer != "":
-            smer = input("Zadej příkaz (sever | východ | jih | západ) nebo ENTER pro ukončení: ")
+            print(self.pozice)
+            smer = input(f"Zadej příkaz ({" | ".join(self.pozice.smery)}) nebo ENTER pro ukončení: ")
             print(150 * '=')
-            if smer not in ["sever", "jih", "západ", "východ", ""]:
-                print("Můj vstupní slovník neobsahuje tento příkaz.")
-            if self.pozice.kontrola(smer):
-                print(self.pohyb(smer))
+            if smer in self.pozice.smery:
+                self.pohyb(smer)
             elif smer == "":
                 print("Pohyb ukončen")
             else:
-                print("Tímto směrem nelze jít.")
+                print("Tímto směrem nelze jít. Zůstáváš na pozici:")
+
+
 
 
 # Zahájení hry
 hra = Hra()
-print(hra.pozice)
 hra.interakce()
